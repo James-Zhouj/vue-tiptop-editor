b7546e08cf49c90dc00ba4046dbc18dcc3bf2441
zhouj <767996455@qq.com>
2022-11-16 20:43:08 +0800

    整理代码

---
+++ packages/index.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    setup() {
+        return () => "我的第一个开源项目"
+    }
+})
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import VueTiptopEditor from "../packages/index"
 </script>
 
 <template>
-  <div>
-    Hello James Zhou
-  </div>
+  <VueTiptopEditor></VueTiptopEditor>
 </template>
 
 <style scoped>
