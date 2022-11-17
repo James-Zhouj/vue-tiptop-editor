a46c0f4af7b8e4e79ac4568dd8250a141340976e
zhouj <767996455@qq.com>
2022-11-17 21:48:35 +0800

    头部组件

---
+++ packages/header/index.tsx
@@ -0,0 +1,19 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+
+    setup() {
+        return () => <div>
+
+            <div>加粗</div>
+            <div>斜体</div>
+            <div>划除</div>
+            <div>代码</div>
+            <div>高亮</div>
+            <div>划除</div>
+            <div>划除</div>
+
+        </div>
+    }
+
+})
