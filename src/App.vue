6476ac30edd7041586206b5193fa50ab8ebddc76
zhouj <767996455@qq.com>
2022-11-18 21:23:58 +0800

    特测双向绑定值

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from "vue";
 import VueTiptopEditor from "../packages/index"
+
+const editorValue = ref("1111222222")
+
+const getEditorValue = () => {
+  console.log("提取内容", editorValue.value);
+  
+}
 </script>
 
 <template>
-  <VueTiptopEditor></VueTiptopEditor>
+  <VueTiptopEditor v-model="editorValue"></VueTiptopEditor>
+  <button @click="getEditorValue">提取富文本内容</button>
 </template>
 
 <style scoped>
