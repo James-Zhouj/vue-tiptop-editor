dab5385238be85c8ed2c5dc466f4586d24210526
zhouj <767996455@qq.com>
2022-11-18 20:26:28 +0800

    封装主题展示部分

--- packages/content/index.tsx
+++ packages/content/index.tsx
@@ -1,26 +1,20 @@
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, onMounted, PropType, computed } from "vue";
 import "./style.scss"
 import { Editor, EditorContent } from '@tiptap/vue-3'
-import StarterKit from '@tiptap/starter-kit'
 
 export default defineComponent({
-    setup() {
+    props:{
+        editor:{
+            type: Object as PropType<Editor | undefined> ,
+            required: true
+        }
+    },
+    setup(props) {
 
-        const editor = ref<Editor>()
-        onMounted(() => {
-            console.log("onMounted")
-            editor.value = new Editor({
-                extensions: [ StarterKit ],
-                content: "",
-                onUpdate: () => {
-                    // emit('update:modelValue', editor.value?.getHTML())
-                },
-            })
+        const _editor = computed( () => props.editor ) 
 
-            console.log("editor onMounted", editor)
-        })
-
-        return () => <EditorContent class="content" editor={editor.value} />
+        
+        return () => <EditorContent class="content" editor={_editor.value} />
     }
 
 })
