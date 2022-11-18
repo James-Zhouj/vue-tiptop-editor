c71c2dadeaf0231d8d05c82e741cd70a174e9d2d
zhouj <767996455@qq.com>
2022-11-18 20:26:49 +0800

    封装editor对象

--- packages/index.tsx
+++ packages/index.tsx
@@ -2,12 +2,30 @@ import { defineComponent, onMounted, ref } from "vue";
 import "./index.scss"
 import Header from "./header"
 import Content from "./content"
-
+import { Editor } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import Highlight from '@tiptap/extension-highlight'
 export default defineComponent({
     setup() {
+
+
+
+        const editor = ref<Editor>()
+        onMounted(() => {
+            console.log("onMounted")
+            editor.value = new Editor({
+                extensions: [ StarterKit, Highlight ],
+                content: "",
+                onUpdate: () => {
+                    // emit('update:modelValue', editor.value?.getHTML())
+                },
+            })
+            console.log("editor onMounted", editor)
+        })
+
         return () => <div class="editor-container" style="height:300px">
-            <Header class="editor-container__header"></Header>
-            <Content class="editor-container___content"></Content>
+            <Header class="editor-container__header" editor={editor.value}></Header>
+            <Content class="editor-container___content" editor={editor.value}></Content>
         </div>
     }
 })
